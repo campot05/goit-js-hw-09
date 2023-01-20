c78148c090c86f26a8d84b2bf01218629fbb3ce6
campot05 <daniil.spectr@gmail.com>
2023-01-20 19:24:18 +0200

    task1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+  start: document.querySelector('[data-start]'),
+  stop: document.querySelector('[data-stop]'),
+  body: document.body,
+};
+refs.stop.setAttribute('disabled', 'disabled');
+let interval;
 
+refs.start.addEventListener('click', changeBgColor);
+refs.stop.addEventListener('click', stopChangeBgColor);
+
+function changeBgColor() {
+  if (!interval) {
+    refs.stop.removeAttribute('disabled');
+    refs.start.setAttribute('disabled', 'disabled');
+    interval = setInterval(setBgColor, 800);
+  }
+}
+
+function setBgColor() {
+  refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+}
+
+function stopChangeBgColor() {
+  refs.stop.setAttribute('disabled', 'disabled');
+  refs.start.removeAttribute('disabled');
+  clearInterval(interval);
+  interval = null;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+// refs.start.setAttribute('disabled', 'disabled');
+// refs.stop.setAttribute('disabled', 'disabled');
